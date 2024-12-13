248dcb6d6f4a9f99d8f5cf249cea182408635ab4
deepusharma701509 <123170251+deepusharma701509@users.noreply.github.com>
2024-12-13 13:02:38 +0530

    Update calc.js

--- miniProject/calc.js
+++ miniProject/calc.js
@@ -7,43 +7,81 @@ const delButton = document.querySelector('input[value="DEL"]');
 let currentInput = "";
 
 
-function handleButtonClick(buttonValue) {
-  if (buttonValue === 'DEL') {
-    currentInput = currentInput.slice(0, -1); 
-  } else if (buttonValue === 'RESET') {
-    currentInput = ""; 
-  } else if (buttonValue === '=') {
-    try {
-     
-      currentInput = eval(currentInput) || ""; 
-    } catch (error) {
-      currentInput = "Error"; 
-    }
-  } else {
-    currentInput += buttonValue; 
-  }
+function handleButtonClick(buttonValue, callback) {
+  
+  currentInput = callback(currentInput, buttonValue);
+  
   
   numInput.value = currentInput;
 }
 
 
+function updateInput(currentInput, buttonValue) {
+  if (buttonValue === 'DEL') {
+    
+    return currentInput.slice(0, -1);
+  } else if (buttonValue === 'RESET') {
+    
+    return "";
+  } else if (buttonValue === '=') {
+ 
+    try {
+      return evaluateExpression(currentInput);
+    } catch (error) {
+      return "Error"; 
+    }
+  } else {
+    
+    return currentInput + buttonValue;
+  }
+}
+
+
+function evaluateExpression(expression) {
+  
+  expression = expression.replace(/X/g, '*').replace(/÷/g, '/');
+
+ 
+  if (!isValidExpression(expression)) {
+    return "Error"; 
+  }
+  
+  
+  try {
+    return Function('return ' + expression)(); 
+  } catch (e) {
+    return "Error"; 
+  }
+}
+
+
+function isValidExpression(expression) {
+ 
+  const regex = /^[0-9+\-*/.()]+$/;
+  return regex.test(expression);
+}
+
+
 buttons.forEach(button => {
   button.addEventListener('click', function() {
-    handleButtonClick(this.value); 
+    const buttonValue = this.value;
+    
+ 
+    handleButtonClick(buttonValue, updateInput);
   });
 });
 
 
 resetButton.addEventListener('click', function() {
-  handleButtonClick('RESET');
+  handleButtonClick('RESET', updateInput);
 });
 
 
 delButton.addEventListener('click', function() {
-  handleButtonClick('DEL');
+  handleButtonClick('DEL', updateInput);
 });
 
 
 equalButton.addEventListener('click', function() {
-  handleButtonClick('=');
+  handleButtonClick('=', updateInput);
 });
